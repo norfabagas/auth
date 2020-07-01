a880969983f582f1452c7b047fd76b64ffb05abc
Norfa Bagas <bgsnorfa@gmail.com>
2020-07-01 15:34:50 +0700

    Adjust login json response

--- api/controllers/login_controller.go
+++ api/controllers/login_controller.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/norfabagas/auth/api/auth"
 	"github.com/norfabagas/auth/api/models"
 	"github.com/norfabagas/auth/api/responses"
+	"github.com/norfabagas/auth/api/utils/crypto"
 	"github.com/norfabagas/auth/api/utils/formaterror"
 )
 
@@ -47,11 +49,34 @@ func (server *Server) Login(w http.ResponseWriter, r *http.Request) {
 		responses.ERROR(w, http.StatusUnprocessableEntity, err)
 		return
 	}
+
 	token, err := server.SignIn(user.Email, user.Password)
 	if err != nil {
 		formattedError := formaterror.FormatError(err.Error())
 		responses.ERROR(w, http.StatusUnprocessableEntity, formattedError)
 		return
 	}
-	responses.JSON(w, http.StatusOK, true, http.StatusText(http.StatusOK), token)
+
+	err = server.DB.Debug().Model(models.User{}).Where("email = ?", user.Email).Take(&user).Error
+	if err != nil {
+		formattedError := formaterror.FormatError(err.Error())
+		responses.ERROR(w, http.StatusUnprocessableEntity, formattedError)
+		return
+	}
+
+	user.Name, err = crypto.Decrypt(user.Name, os.Getenv("APP_KEY"))
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	responses.JSON(w, http.StatusOK, true, http.StatusText(http.StatusOK), struct {
+		Token string `json:"token"`
+		Email string `json:"email"`
+		Name  string `json:"name"`
+	}{
+		Token: token,
+		Email: user.Email,
+		Name:  user.Name,
+	})
 }
