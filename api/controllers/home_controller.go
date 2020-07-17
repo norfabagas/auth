55c4e23b1e2e64f9cb0cf14138a5ed0f4f58b0df
Norfa Bagas <bgsnorfa@gmail.com>
2020-07-17 14:49:08 +0700

    Add new endpoint to get api_secret key

--- api/controllers/home_controller.go
+++ api/controllers/home_controller.go
@@ -2,10 +2,15 @@ package controllers
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/norfabagas/auth/api/responses"
 )
 
 func (server *Server) Home(w http.ResponseWriter, r *http.Request) {
 	responses.JSON(w, http.StatusOK, true, http.StatusText(http.StatusOK), "Index")
+}
+
+func (server *Server) ApiSecret(w http.ResponseWriter, r *http.Request) {
+	responses.JSON(w, http.StatusOK, true, http.StatusText(http.StatusOK), os.Getenv("API_SECRET"))
 }
--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -4,6 +4,9 @@ import "github.com/norfabagas/auth/api/middlewares"
 
 func (s *Server) InitializeRoutes() {
 	s.Router.HandleFunc("/", middlewares.SetMiddlewareJSON(s.Home)).Methods("GET")
+
+	s.Router.HandleFunc("/api-secret", middlewares.SetMiddlewareJSON(s.ApiSecret)).Methods("GET")
+
 	s.Router.HandleFunc("/login", middlewares.SetMiddlewareJSON(s.Login)).Methods("POST")
 
 	s.Router.HandleFunc("/register", middlewares.SetMiddlewareJSON(s.CreateUser)).Methods("POST")
